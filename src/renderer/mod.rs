c132fd89917d73d4c32c3f2ad15717e982edccfb
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2026-02-15 12:29:27 +0100

    Max depth 5

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -305,7 +305,7 @@ impl<'a> Renderer<'a> {
             width,
             height,
             sample_count: 0,
-            max_depth: 50,
+            max_depth: 5,
             background_color: [
                 scene.background_color.x as f32,
                 scene.background_color.y as f32,
